2b3f16f6e5585967925906eac3b193c13e57bcd9
jahangiralam7864 <wedevs95@gmail.com>
2022-02-21 15:42:24 +0600

    push

--- basic.js
+++ basic.js
@@ -1,49 +1,49 @@
 
-// // variable declare 
-// var deposit = 400;
-// // condition check
-// if (deposit > 500) {
-//     alert("Hello! I am an alert box!!");
-// }if (deposit < 200) {
-//     alert("I love my country!!");
-// }if (deposit == 500) {
-//     alert("Hello! I am an alert box!!");
-// }if (deposit != 500) {
-//     alert("Hello! I am an alert box!!");    
-// }if (deposit >= 500) {
-//     alert("Hello! I am an alert box!!");
-// }if (deposit <= 500) {
-//     alert("Hello! I am an alert box!!");
-// }else {
-//     alert("md jahangir alam!!");
-// }
-// // array 
-// const numbers = [23,35,658,100,20];
-// const numberCount = numbers.length;
-// numbers.push(120);
-// numbers.pop();
-// numbers[2] = 555;
-// // check whether 222 included in the array
-// if (numbers.indexOf(222) != -1) {
-//     // not value
-// }
-// if (numbers.includes(222)) {
-//     // right value
-// }
-// // loop / while / for
-// const iterable = [10, 20, 30];
+// variable declare 
+var deposit = 400;
+// condition check
+if (deposit > 500) {
+    alert("Hello! I am an alert box!!");
+}if (deposit < 200) {
+    alert("I love my country!!");
+}if (deposit == 500) {
+    alert("Hello! I am an alert box!!");
+}if (deposit != 500) {
+    alert("Hello! I am an alert box!!");    
+}if (deposit >= 500) {
+    alert("Hello! I am an alert box!!");
+}if (deposit <= 500) {
+    alert("Hello! I am an alert box!!");
+}else {
+    alert("md jahangir alam!!");
+}
+// array 
+const numbers = [23,35,658,100,20];
+const numberCount = numbers.length;
+numbers.push(120);
+numbers.pop();
+numbers[2] = 555;
+// check whether 222 included in the array
+if (numbers.indexOf(222) != -1) {
+    // not value
+}
+if (numbers.includes(222)) {
+    // right value
+}
+// loop / while / for
+const iterable = [10, 20, 30];
 
-// for (let value of iterable) {
-//   value += 1;
-//   console.log(value); //result = 11,21,31
-// }
-// // function
-// function fullName(first,second) {
-//     const names = first+ ' ' + second;
-// }
-// const person = fullName('jahangir', 'joy');
-// // object 
-// const bottle = {color: 'yellow', contain: 'water',price: '50'}
+for (let value of iterable) {
+  value += 1;
+  console.log(value); //result = 11,21,31
+}
+// function
+function fullName(first,second) {
+    const names = first+ ' ' + second;
+}
+const person = fullName('jahangir', 'joy');
+// object 
+const bottle = {color: 'yellow', contain: 'water',price: '50'}
 
 // function a click kore webpage style korbo.
 document.getElementById('add-border').addEventListener('click',function () {
@@ -70,3 +70,10 @@ function () {
     `;
     container.appendChild(friendDiv);
 })
+//for 
+const number = [10,20,30,40,50];
+for (let i = 0; i < number.length; i++) {
+    const num = number[i];
+    console.log(num);  
+}
+
